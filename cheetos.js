8f2605fb54540e70f1177608398f1ffa0eefd571
Philly Kelly <p_a_k_@hotmail.com>
2017-12-21 02:02:56 +0000

    automatically backed up by learn

--- cheetos.js
+++ cheetos.js
@@ -5,7 +5,17 @@ var meals = {
   lunch: 'eggs',
   dinner: 'cheetos'
 };
-
+var vegetables = {
+  fav: 'broccoli',
+  gross: 'peas'
+}
 meals['second breakfast'] = 'bagel'
 
 console.log(meals['second breakfast'])
+
+
+function updateObjectWithObject(targetObject, updatesObject) {
+  return Object.assign({}, targetObject, updatesObject)
+}
+
+updateObjectWithObject(meals, vegetables)
